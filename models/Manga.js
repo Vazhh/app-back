98ee25471e60dfffd15fae841481e1a3c23f2f12
Vazhh <rg.aguirre93@gmail.com>
2023-05-16 21:28:22 -0500

    cargue de data.js

--- models/Manga.js
+++ models/Manga.js
@@ -2,8 +2,8 @@ import { Schema, model, Types} from 'mongoose'
 
 let collection = 'mangas'
 let schema = new Schema({
-    author_id: {type:Types.ObjectId,ref:authors,required:true},
-    company_id: {type:String},
+    author_id: {type:Types.ObjectId,ref:'authors',required:true},
+    company_id: {type:Types.ObjectId,ref:'companies'},
     title: {type:String,required:true},
     cover_photo: {type:String,required:true},
     description: {type:String,required:true},
