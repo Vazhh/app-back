048492f9024afbbd950ac8db89071090d131627f
Gerardo  Aguirre <95187790+Vazhh@users.noreply.github.com>
2023-05-16 19:53:57 -0500

    modelo author (#3)

---
+++ models/data/authors.js
@@ -0,0 +1,45 @@
+import 'dotenv/config.js'
+import '../../config/database.js'
+import Author from '../Author.js'
+
+let authors = [{
+    name: "alejandro",
+    city: "buenos aires",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '64641ad93c69705be1f258b5'
+},{
+    name: "lucas",
+    last_name: "silva",
+    city: "buenos aires",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '64641ad93c69705be1f258b6'
+},{
+    name: "jose",
+    last_name: "lopez",
+    city: "villa carlos paz",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '64641ad93c69705be1f258b7'
+},{
+    name: "eric",
+    city: "resistencia",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '64641ad93c69705be1f258b8'
+},{
+    name: "igna",
+    last_name: "borraz",
+    city: "rosario",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '64641ad93c69705be1f258b9'
+}]
+
+Author.insertMany(authors)
