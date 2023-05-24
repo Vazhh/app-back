5f5477fa0202eee33d9e3c7aa7cb0609be835755
Gerardo  Aguirre <95187790+Vazhh@users.noreply.github.com>
2023-05-23 21:47:15 -0500

    router-controller (#7)

---
+++ router/chapters.js
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import read from '../controllers/chapters/read.js'
+
+let chaptersRouter = Router()
+
+
+chaptersRouter.get('/',read)
+
+export default chaptersRouter
