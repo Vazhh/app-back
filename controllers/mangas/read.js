0c98b28ec7d4c152618fb05c3881b900d0e8083c
Gerardo  Aguirre <95187790+Vazhh@users.noreply.github.com>
2023-06-22 23:27:50 -0500

    M02 endpoint (#13)
    * Se crea  create endpoint de mangas
    
    * fix

--- controllers/mangas/read.js
+++ controllers/mangas/read.js
@@ -1,7 +1,23 @@
-export default (req,res)=>{
-    
-    return res.status(200).json({
-        success:true,
-        message:'User'
-    })
+import Manga from '../../models/Manga.js'
+
+export default async(req,res)=>{
+    try {
+        let all = await Manga.find()
+        if(all){
+            return res.status(200).json({
+                response: all,
+                message: 'Mangas found!'
+            })
+        }else{
+            return res.status(400).json({
+                response: null,
+                message: 'Mangas not found'
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({
+            response: null,
+            message: 'error'
+        })
+    }
 }
