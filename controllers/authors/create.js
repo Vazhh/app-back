41d7e0bfa76233501577d84355b70697a859e06b
Gerardo  Aguirre <95187790+Vazhh@users.noreply.github.com>
2023-06-22 20:57:06 -0500

    Se crea  create endpoint de mangas (#12)

--- controllers/authors/create.js
+++ controllers/authors/create.js
@@ -1,23 +1,19 @@
 import Author from '../../models/Author.js'
 export default async(req,res)=>{
     try {
-        let data = req.body // el cliente envia un objeto en la propiedad body del objeto req (requerimientos)
-        let one = Author.create(data)// el objeto data deberia contener TODOS los datos necesarios para crear algo
-        if (one){
+        let one = await Author.create(req.body)// el objeto data deberia contener TODOS los datos necesarios para crear algo
+       
             return res.status(200).json({
+                success:true,
                 response:one,
                 message:'created'
             })
-        }else{
-            return res.status(400).json({
-                response:null,
-                message:'not created'
-            })
-        }
+        
     } catch (error) {
         return res.status(500).json({
+            success: false,
             response: null,
-            message: 'not create'
+            message: 'not created'
         })
     }
 }
---
+++ controllers/mangas/create.js
@@ -0,0 +1,18 @@
+import Manga from '../../models/Manga.js'
+export default async(req,res)=>{
+    try {
+        let one = await Manga.create(req.body)// el objeto data deberia contener TODOS los datos necesarios para crear algo
+            return res.status(200).json({
+                success:true,
+                response:one,
+                message:'created'
+            })
+        
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            response: null,
+            message: 'not created'
+        })
+    }
+}
