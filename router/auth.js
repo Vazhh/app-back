a5ed351ffc331f19292becc4c6c48940e8aaa100
ignacioborraz <ignacioborraz@hotmail.com>
2023-06-25 21:09:46 -0300

    signin

--- router/auth.js
+++ router/auth.js
@@ -1,19 +1,24 @@
 //controllers
 import register from "../controllers/auth/register.js"
+import signin from "../controllers/auth/signin.js"
 import read from "../controllers/auth/read.js"
 
 //middlewares
 import validator from "../middlewares/validator.js"
 import accountExistsSignUp from "../middlewares/accountExistsSignUp.js"
+import accountExistsSignIn from "../middlewares/accountExistsSignIn.js"
 
 //schemas
 import register_schema from "../schemas/auth/register.js"
+import signin_schema from "../schemas/auth/signin.js"
+import isValidPassword from "../middlewares/isValidPassword.js"
 
 //router
 import { Router } from "express"
 let authRouter = Router()
 
 authRouter.post('/register', validator(register_schema), accountExistsSignUp, register)
+authRouter.post('/signin', validator(signin_schema), accountExistsSignIn, isValidPassword, signin)
 authRouter.get('/', read)
 
 export default authRouter
