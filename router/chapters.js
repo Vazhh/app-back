6da9926cfeba1310e5e0f97df6c5eb9a902d6ed9
Guillermo Duque <guille980627@gmail.com>
2023-07-06 22:44:55 -0500

    se crea schema y se enruta

--- router/chapters.js
+++ router/chapters.js
@@ -8,10 +8,21 @@ import is_property_of from '../middlewares/is_property_of.js'
 import exists_order from '../middlewares/exists_order.js'
 import next_order from '../middlewares/next_order.js'
 import validator from '../middlewares/validator.js'
+import add_cover_photo from "../middlewares/add_cover_photo.js"
+import create_schema from "../schemas/chapters/create.js"
 
 let chaptersRouter = Router()
 
 chaptersRouter.get('/',read)
-chaptersRouter.post('/',create)
+chaptersRouter.post('/',
+    passport.authenticate('jwt',{ session:false }),
+    has_permition,
+    is_active,
+    is_property_of,
+    validator(create_schema),
+    exists_order,
+    next_order,
+    add_cover_photo,
+create)
 
 export default chaptersRouter
