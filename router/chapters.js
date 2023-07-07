f237984a9fad509fc07e882abba17f220e99a5ff
Guillermo Duque <guille980627@gmail.com>
2023-07-06 19:12:40 -0500

    se importan middlewares

--- router/chapters.js
+++ router/chapters.js
@@ -1,6 +1,13 @@
 import { Router } from 'express'
 import read from '../controllers/chapters/read.js'
 import create from '../controllers/chapters/create.js'
+import passport from 'passport'
+import has_permition from '../middlewares/has_permition.js'
+import is_active from '../middlewares/is_active.js'
+import is_property_of from '../middlewares/is_property_of.js'
+import exists_order from '../middlewares/exists_order.js'
+import next_order from '../middlewares/next_order.js'
+import validator from '../middlewares/validator.js'
 
 let chaptersRouter = Router()
 
