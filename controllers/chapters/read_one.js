15034ee08c47d0ad42353f1fb711eea1c5dcb21a
Vazhh <rg.aguirre93@gmail.com>
2023-07-17 18:35:58 -0500

    fix-one-chapter

--- controllers/chapters/read_one.js
+++ controllers/chapters/read_one.js
@@ -6,14 +6,10 @@ export default async (req, res, next) => {
     console.log(id)
     let one = await Chapter.findById(id,"title cover_photo pages -_id");
     const nextChapter = await Chapter.findOne({ _id: { $gt: id } });
-    const prevChapter = await Chapter.findOne({ _id: { $lt: id } });
     return res.status(200).json({
       success: true,
-      prev: prevChapter ? prevChapter._id : null,
       next: nextChapter ? nextChapter._id : null,
       response: one,
-      prev: prevChapter ? prevChapter._id : null,
-      next: nextChapter ? nextChapter._id : null,
       messages: ["Found"],
       
     });
