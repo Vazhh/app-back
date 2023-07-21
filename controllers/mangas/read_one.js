fb93bee1398cd4743703247ef5259a5e7a5d3a7a
natigalo <natigalo1@gmail.com>
2023-07-20 21:34:31 -0500

    fix"readOne"

--- controllers/mangas/read_one.js
+++ controllers/mangas/read_one.js
@@ -4,7 +4,7 @@ export default async(req,res,next)=>{
  try {
    let id= req.params.id;
    console.log(id) 
-   let one= await Manga.findById(id, 'title cover_photo description category_id -_id').populate('category_id', 'name -_id' );
+   let one= await Manga.findById(id, 'title cover_photo author_id description category_id -_id').populate('category_id', 'name color hover -_id' ).populate('author_id', 'name');
    
    return res.status(200).json({
      success: true,
