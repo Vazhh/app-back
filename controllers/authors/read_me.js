8ebc9251eb04ecb16440c59a3da361b09b3b0bda
Guillermo Leon Duque Castrillon <63427410+GuillermoDuKe@users.noreply.github.com>
2023-08-05 11:07:40 -0500

    Update read_me.js

--- controllers/authors/read_me.js
+++ controllers/authors/read_me.js
@@ -4,14 +4,13 @@ import Author from '../../models/Author.js';
 export default async (req, res, next) => {
   try {
 
-    const user = req.user; // CAPTURO DATOS DE USUARIO
-    let all = await Author.findOne({ user_id: user._id }, 'photo name country city -_id'); // CAPTURO DATOS DE AUTHOR
-
+    const profile = req.user; // CAPTURO DATOS DE USUARIO
+    let all = await Author.findOne({ user_id: profile._id }, 'photo name country city -_id'); // CAPTURO DATOS DE AUTHOR
     return res.status(200).json({
       success: true,
       response: { 
-        all, 
-        profile: user 
+        // all, 
+        profile: all
       },
       message: 'Author found'
     });
